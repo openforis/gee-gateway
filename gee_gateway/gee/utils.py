12a66e484ee5d0a5bb5bc5210b9e85f3394e8068
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2016-12-20 12:54:29 +0100

    new endpoint ndvi used to get the NDVI values for a specific image collection, date range and polygon

--- gee_gateway/gee/utils.py
+++ gee_gateway/gee/utils.py
@@ -28,3 +28,21 @@ def fistImageInMosaicToMapId(collectionName, visParams={}, dateFrom=None, dateTo
     except EEException as e:
         raise GEEException(e.message)
     return values
+
+def calculateNdvi(collectionName, scale, polygon=[], dateFrom=None, dateTo=None):
+    """  """
+    try:
+        plot = ee.Geometry.Polygon(polygon)
+        ndviCollection = ee.ImageCollection(collectionName).filterDate(dateFrom, dateTo).select('NDVI')
+        def getNdvi(image):
+            """  """
+            ndviValue = image.reduceRegion(ee.Reducer.mean(), plot, scale).get('NDVI')
+            date = image.get('system:time_start')
+            ndviImage = ee.Image().set('ndvi', [ndviValue, ee.Number(date)])
+            return ndviImage
+        ndviCollection1 = ndviCollection.map(getNdvi)
+        ndviCollection2 = ndviCollection1.aggregate_array('ndvi')
+        values = ndviCollection2.getInfo()
+    except EEException as e:
+        raise GEEException(e.message)
+    return values
--- gee_gateway/web/routes.py
+++ gee_gateway/web/routes.py
@@ -20,11 +20,11 @@ def image():
     .. code-block:: javascript
 
         {
-           imageName: "XXX",
-           visParams: {
-               min: 0.0,
-               max: 0.0,
-               bands: "XX,XX,XX"
+            imageName: "XXX",
+            visParams: {
+                min: 0.0,
+                max: 0.0,
+                bands: "XX,XX,XX"
            }
         }
 
@@ -33,8 +33,8 @@ def image():
     .. code-block:: javascript
 
         {
-           mapid: "XXX",
-           token: "XXX"
+            mapid: "XXX",
+            token: "XXX"
         }
 
     :reqheader Accept: application/json
@@ -68,14 +68,14 @@ def imageByMosaicCollection():
     .. code-block:: javascript
 
         {
-           collectionName: "XX",
-           visParams: {
-               min: 0.0,
-               max: 0.0,
-               bands: "XX,XX,XX"
-           },
-           dateFrom: "YYYY-MM-DD",
-           dateTo: "YYYY-MM-DD"
+            collectionName: "XX",
+            visParams: {
+                min: 0.0,
+                max: 0.0,
+                bands: "XX,XX,XX"
+            },
+            dateFrom: "YYYY-MM-DD",
+            dateTo: "YYYY-MM-DD"
         }
 
     **Example response**:
@@ -83,8 +83,8 @@ def imageByMosaicCollection():
     .. code-block:: javascript
 
         {
-           mapid: "XXX",
-           token: "XXX"
+            mapid: "XXX",
+            token: "XXX"
         }
 
     :reqheader Accept: application/json
@@ -104,6 +104,62 @@ def imageByMosaicCollection():
                 dateFrom = json.get('dateFrom', None)
                 dateTo = json.get('dateTo', None)
                 values = fistImageInMosaicToMapId(collectionName, visParams, dateFrom, dateTo)
+    except GEEException as e:
+        logger.error(e.message)
+        values = {
+            'errMsg': e.message
+        }
+    return jsonify(values), 200
+
+@app.route('/ndvi', methods=['POST'])
+@cross_origin(origins=app.config['CO_ORIGINS'])
+def ndvi():
+    """
+    .. :quickref: NDVI; Get the NDVI values for a specific ImageCollection, date range and polygon
+
+    **Example request**:
+
+    .. code-block:: javascript
+
+        {
+            collectionName: "XX",
+            scale: 0.0,
+            polygon: [
+                [0.0, 0.0],
+                [...]
+            ],
+            dateFrom: "YYYY-MM-DD",
+            dateTo: "YYYY-MM-DD"
+        }
+
+    **Example response**:
+
+    .. code-block:: javascript
+
+        [
+            [0, 0],
+            ...
+        ]
+
+    :reqheader Accept: application/json
+    :<json String collectionName: name of the image collection
+    :<json Float scale: scale in meters of the projection
+    :<json Array polygon: the region over which to reduce data
+    :<json String dateFrom: start date
+    :<json String dateTo: end date
+    :resheader Content-Type: application/json
+    """
+    values = {}
+    try:
+        json = request.get_json()
+        if json:
+            collectionName = json.get('collectionName', None)
+            polygon = json.get('polygon', None)
+            scale = json.get('scale', 30)
+            if collectionName and polygon:
+                dateFrom = json.get('dateFrom', None)
+                dateTo = json.get('dateTo', None)
+                values = calculateNdvi(collectionName, scale, polygon, dateFrom, dateTo)
     except GEEException as e:
         logger.error(e.message)
         values = {
