d44035375327dcaecc06cf13adddef8e883787b0
Roberto Fontanarosa <robertofontanarosa@gmail.com>
2016-12-21 15:09:49 +0100

    a demo page has been added

--- run.py
+++ run.py
@@ -8,4 +8,8 @@ if __name__ == '__main__':
     logging.getLogger('flask_cors').level = app.config['LOGGING_LEVEL']
     logging.getLogger('gee_gateway').level = app.config['LOGGING_LEVEL']
 
+    @app.route('/')
+    def root():
+        return app.send_static_file('index.html')
+
     app.run(debug=app.config['DEBUG'], port=app.config['PORT'], host=app.config['HOST'])
