1f9524af2c4003c15cd7afecce8300f9589f36e5
Billy Ashmall <billy.ashmall@nasa.gov>
2018-10-29 11:27:31 -0500

    added https

--- run.py
+++ run.py
@@ -32,4 +32,4 @@ if __name__ == '__main__':
     logging.getLogger('flask_cors').level = app.config['LOGGING_LEVEL']
     logging.getLogger('gee_gateway').level = app.config['LOGGING_LEVEL']
 
-    app.run(debug=app.config['DEBUG'], port=app.config['PORT'], host=app.config['HOST'])
+    app.run(debug=app.config['DEBUG'], port=app.config['PORT'], host=app.config['HOST'], ssl_context='adhoc')
