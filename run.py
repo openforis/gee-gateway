a70e2df4f35db6aa13d88c43b61b4fa4babec37f
Billy Ashmall <billy.ashmall@nasa.gov>
2018-11-02 11:46:52 -0500

    enable https

--- run.py
+++ run.py
@@ -32,5 +32,5 @@ if __name__ == '__main__':
     logging.getLogger('flask_cors').level = app.config['LOGGING_LEVEL']
     logging.getLogger('gee_gateway').level = app.config['LOGGING_LEVEL']
 
-    app.run(debug=app.config['DEBUG'], port=app.config['PORT'], host=app.config['HOST'])
-    #app.run(debug=app.config['DEBUG'], port=app.config['PORT'], host=app.config['HOST'], ssl_context='adhoc')
+    #app.run(debug=app.config['DEBUG'], port=app.config['PORT'], host=app.config['HOST'])
+    app.run(debug=app.config['DEBUG'], port=app.config['PORT'], host=app.config['HOST'], ssl_context='adhoc')
