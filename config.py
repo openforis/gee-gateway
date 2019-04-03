6ad919aec85c9ca77b0a6fc6c193eefb150b7cb1
Billy Ashmall <billy.ashmall@nasa.gov>
2019-04-03 15:31:01 -0500

    config

---
+++ config.py
@@ -0,0 +1,13 @@
+DEBUG = False
+PORT = 8888
+HOST = '0.0.0.0'
+CERT = '/etc/letsencrypt/live/ceodev.servirglobal.net/fullchain.pem'
+KEY = '/etc/letsencrypt/live/ceodev.servirglobal.net/privkey.pem'
+
+CO_ORIGINS = '*'
+
+EE_ACCOUNT = '945396317946-ofvpdldm70jk29d1d4pccu0dhsvkpn8s@developer.gserviceaccount.com'
+EE_KEY_PATH = '/home/openforis/nginx_temp/privatekeyCE.pem'
+
+import logging
+LOGGING_LEVEL = logging.INFO
