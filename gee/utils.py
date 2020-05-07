3f415d04bbaad05e18e27fce81cdc2281b6e3590
Billy Ashmall <billy.ashmall@nasa.gov>
2020-05-07 10:12:56 -0500

    add getDegraditionTileUrl

--- gee/utils.py
+++ gee/utils.py
@@ -541,8 +541,8 @@ def getDegraditionTileUrlByDate(geometry, date):
         logger.error("making point")
         geometry = ee.Geometry.Point(geometry)
     landsatData = gee.inputs.getLandsat({
-        "start": befDate.getInfo()['value'],
-        "end": aftDate.getInfo()['value'],
+        "start": str(befDate.getInfo()['value']),
+        "end": str(aftDate.getInfo()['value']),
         "targetBands": ['RED','GREEN','BLUE'],
         "region": geometry,
         "sensors": {"l4": False, "l5": False, "l7": False, "l8": True}
