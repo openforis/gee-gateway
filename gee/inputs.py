7a488d71db53635c96d841880ba4440a54cd3a6f
John Dilger <johnjdilger@gmail.com>
2020-05-14 13:43:07 -0700

    added new get sentinel1 function

--- gee/inputs.py
+++ gee/inputs.py
@@ -343,23 +343,57 @@ def prepare(orbit):
     .filter(ee.Filter.eq('instrumentMode', 'IW')) \
     .filter(ee.Filter.eq('orbitProperties_pass', orbit))
 
-def getS1(mode, focalSize):
-    if focalSize is None:
-        focalSize = 3
-    if mode is None:
-        mode = 'ASCENDING'
+def getS1Alt(mode, focalSize, options):
+    if options is None:
+        pass
+    else:
+        if 'targetBands' in options:
+            targetBands = options['targetBands']
+        else:
+            targetBands = ['VV','VH','VH/VV']
+        if 'focalSize' in options:
+            focalSize = options['focalSize']
+        else:
+            focalSize = None
+        if mode in options:
+            mode = options['mode']
+        else:
+            mode = 'ASCENDING'
+        if 'region' in options:
+            region = options['region']
+        else:
+            region = None
+
     def s1Mapper(img):
         fmean = img.add(30).focal_mean(focalSize)
-        ratio = fmean.select('VH').divide(fmean.select('VV')).rename('ratio').multiply(30)
-        return img.select().addBands(fmean).addBands(ratio)
+        ratio0 = fmean.select('VH').divide(fmean.select('VV')).rename('VH/VV').multiply(30)
+        ratio1 = fmean.select('VV').divide(fmean.select('VH')).rename('VV/VH').multiply(30)
+        return img.select().addBands(fmean).addBands(ratio0)..addBands(ratio1)
+
+    def s1Deg(img):
+        pwr = ee.Image(10).pow(img.divide(10))
+        pwr = pwr.select('VV').subtract(pwr.select('VH')).divide(pwr.select('VV').add(pwr.select('VH'))) \
+            .rename('RFDI')
+        ratio0 = img.select('VV').divide(img.select('VH')).rename('VV/VH')
+        ratio1= img.select('VH').divide(img.select('VV')).rename('VH/VV')
+
+        return img.addBands(pwr).addBands(ratio0).addBands(ratio1)
+
     data = ee.ImageCollection('COPERNICUS/S1_GRD') \
     .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VV')) \
     .filter(ee.Filter.listContains('transmitterReceiverPolarisation', 'VH')) \
+    .filter(ee.Filter.eq('orbitProperties_pass',mode))\
     .filter(ee.Filter.eq('instrumentMode', 'IW')) \
-    .select('V.') \
-    .map(s1Mapper)
 
-    return data
+    if focalSize:
+        data = data.map(s1Mapper)
+    else:
+        data = data.map(s1Deg)
+
+    if region is not None:
+        data = data.filterBounds(region)
+
+    return data.select(targetBands)
 
 def prepareL4L5(image):
     bandList = ['B1', 'B2','B3','B4','B5','B7','B6']
