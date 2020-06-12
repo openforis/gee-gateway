05247315d241d5a1c15259da7f727070a0c08858
BillyZ <billy.ashmall@nasa.gov>
2020-06-12 15:38:23 +0000

    added band option

--- routes.py
+++ routes.py
@@ -1296,6 +1296,8 @@ def getDegraditionTileUrl():
                 visParams = {'bands': 'RED,GREEN,BLUE', 'min': 0, 'max': 1400}
             elif stretch == 543:
                 visParams ={'bands': 'SWIR1,NIR,RED', 'min': 0, 'max': 7000}
+            elif stretch == 453:
+                visParams ={'bands': 'NIR,SWIR1,RED', 'min': 0, 'max': 7000}	     
             elif stretch == "SAR":
                 visParams = {'bands':'VV,VH,VV/VH', 'min':'-15,-25,.40', 'max': '0,-10,1', 'gamma': '1.6' }
             tparams = json.get('visParams', "")
