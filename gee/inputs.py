65adb3be3a6b9d6adeb6d4cb694bc3386f048d86
BillyZ <billy.ashmall@nasa.gov>
2020-07-09 18:14:35 +0000

    fix banding debug

--- gee/inputs.py
+++ gee/inputs.py
@@ -98,7 +98,6 @@ def getLandsat(options):
 
         if region is not None:
             col = col.filterBounds(region)
-        return col
         indices = doIndices(col).select(targetBands)
         if "l5" not in sensors:
             indices = indices.filterMetadata('SATELLITE','not_equals','LANDSAT_5')
